af198d1e46c926c92d0bffbb79dc13928abb4444
Alex Altman <aaltman@cisco.com>
2016-08-22 12:30:27 -0400

    Typoed the final function call from the solution that is passing in the checkio webpage.

--- PyCon-TW-2014/simple_hashlib/script.py
+++ PyCon-TW-2014/simple_hashlib/script.py
@@ -1,4 +1,4 @@
 import hashlib
 
 def checkio(hashed_string, algorithm):
-    return eval('hashlib.' + algorithm)(bytes(hashed_string, 'utf-8'))
+    return eval('hashlib.' + algorithm)(bytes(hashed_string, 'utf-8')).hexdigest()
